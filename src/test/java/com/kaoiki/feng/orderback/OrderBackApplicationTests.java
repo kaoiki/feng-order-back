f739050934545dd2502d773a5be9cd063c2aca0b
kaoiki <zhouc@kaoiki.com>
2023-11-29 08:09:48 +0800

    Initial commit

---
+++ src/test/java/com/kaoiki/feng/orderback/OrderBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaoiki.feng.orderback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
